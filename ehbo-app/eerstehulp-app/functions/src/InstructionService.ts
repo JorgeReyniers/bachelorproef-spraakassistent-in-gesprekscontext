a2c4f64277a8d442a0efb069978b80b88d255025
Jorge Reyniers <jorge.reyniers.w5872@student.hogent.be>
2019-06-07 22:47:45 +0200

    first version of eerste-hulp app

---
+++ ehbo-app/eerstehulp-app/functions/src/InstructionService.ts
@@ -0,0 +1,44 @@
+export default {
+    "fainted":[
+    {
+      "speech":"Controleer het bewustzijn. Schud de schouders en vraag luid: Gaat het?",
+      "text":"Controleer het bewustzijn."
+    },
+    {
+      "speech":"Open de luchtweg.",
+      "text":"Open de luchtweg."
+    },
+    {
+      "speech":"Controleer de ademhaling. Kijk, luister en voel gedurende 10 seconden.",
+      "text":"Controleer de ademhaling. Kijk, luister en voel gedurende 10 seconden."
+    },
+    {
+      "speech":"Laat 1 1 2 bellen of doe dit zelf wanneer je alleen bent.",
+      "text":"Laat 112 bellen of bel zelf 112."
+    },
+    {
+      "speech":"Als het slachtoffer niet ademt, laat iemand een AED halen.",
+      "text":"Laat een AED halen."
+    },
+    {
+      "speech":"Plaats de ene hand in het midden van de borstkas en plaats de andere hand erbovenop en haak de vingers in elkaar.",
+      "text":"Geef 30 borstcompressies."
+    },
+    {
+      "speech":"Duw het borstbeen 5 a 6 centimeter loodrecht naar beneden. Doe dit aan een tempo van 100 a 120 compressies per minuut.",
+      "text":"Geef 30 borstcompressies."
+    },
+    {
+      "speech":"Geef 2 mond-op-mondbeademingen.",
+      "text":"Geef 2 mond-op-mondbeademingen."
+    },
+    {
+      "speech":"Gebruik de AED van zodra die aankomt bij het slachtoffer. Volg de instructies van de AED.",
+      "text":"Volg de instructies van de AED."
+    },
+    {
+      "speech":"Blijf 30 borstcompressies en 2 mond-op-mondbeademingen geven.",
+      "text":"Blijf 30 borstcompressies en 2 mond-op-mondbeademingen geven."
+    }
+  ]
+}
---
+++ ehbo-app/eerstehulp-app/functions/src/index.ts
@@ -0,0 +1,79 @@
+'use strict';
+import {dialogflow, SimpleResponse, Suggestions} from 'actions-on-google/dist/index';
+import * as functions from 'firebase-functions';
+import InstructionService from './InstructionService';
+
+const app = dialogflow({debug: true});
+
+app.intent('Default Welcome Intent', (conv:any) => {
+  conv.ask(new SimpleResponse({
+    speech:"Eerste Hulp hier. Wat is er gebeurd?",
+    text:"Wat is er gebeurd?"
+  }))
+});
+
+app.intent('bewusteloos niet ademend', (conv:any) => {
+  conv.ask(new SimpleResponse({
+    speech:"Luister goed naar mijn instructies. Je kan me steeds vragen om de instructie te herhalen of om de vorige of volgende te horen.",
+    text:"Hier zijn de instructies."
+  }))
+
+  conv.data.step = 0;
+
+  conv.ask(new SimpleResponse({
+    speech: InstructionService.fainted[0].speech,
+    text:InstructionService.fainted[0].text
+  }))
+  conv.ask(new Suggestions(['Vorige','Herhaal','Volgende']));
+})
+
+app.intent('bewusteloos niet ademend - previous', (conv:any) => {
+  if(conv.data.step > 0){conv.data.step--};
+
+  conv.ask(new SimpleResponse({
+    speech:InstructionService.fainted[conv.data.step].speech,
+    text:InstructionService.fainted[conv.data.step].text
+  }))
+  conv.ask(new Suggestions(['Vorige','Herhaal','Volgende']));
+})
+
+app.intent('bewusteloos niet ademend - repeat', (conv:any) => {
+  conv.ask(new SimpleResponse({
+    speech:InstructionService.fainted[conv.data.step].speech,
+    text:InstructionService.fainted[conv.data.step].text
+  }))
+  conv.ask(new Suggestions(['Vorige','Herhaal','Volgende']));
+})
+
+app.intent('bewusteloos niet ademend - next', (conv:any) => {
+  if(conv.data.step+1 < InstructionService.fainted.length){conv.data.step++};
+
+  conv.ask(new SimpleResponse({
+    speech:InstructionService.fainted[conv.data.step].speech,
+    text:InstructionService.fainted[conv.data.step].text
+  }))
+  conv.ask(new Suggestions(['Vorige','Herhaal','Volgende']));
+})
+
+app.intent('Default Fallback Intent', (conv:any) => {
+  conv.ask(new SimpleResponse({
+    speech:"Dat heb ik niet begrepen.",
+    text:"Dat heb ik niet begrepen."
+  }));
+})
+
+app.intent('Default Close Intent', (conv:any) => {
+  conv.ask(new SimpleResponse({
+    speech:"Tot ziens.",
+    text:"Tot ziens."
+  }))
+})
+
+app.catch((conv:any,error:any) => {
+  console.error(error);
+  conv.close("Oeps, er is iets fout gelopen. Onze ontwikkelaars zoeken naar een oplossing.");
+})
+
+exports.yourAction = functions.https.onRequest(app);
+
+
